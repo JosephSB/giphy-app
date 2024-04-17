b951c34bc030da3a046359fa347cf84680d4499f
josephsb <jguku2002@gmail.com>
2024-04-17 13:49:11 -0500

    connect with api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { IGif } from '../../models/gif.model';
+import { GiphyService } from '../../services/giphy.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  providers: [GiphyService],
 })
 export class HomeComponent {
+  giphyService = inject(GiphyService);
 
+  gifs = signal<IGif[]>([]);
+  tags = signal<string[]>([]);
+
+  ngOnInit() {
+    this.getGifs();
+    this.getTrendingTags();
+  }
+
+  getGifs() {
+    this.giphyService.getTrendingGifs().subscribe((data) => {
+      this.gifs.update((prev) => [...prev, ...data]);
+    });
+  }
+
+  getTrendingTags() {
+    this.giphyService.getTrendingTags().subscribe((data) => {
+      this.tags.set(data);
+    });
+  }
 }
---
+++ src/app/services/giphy.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import config from '../config';
+import { Observable, map } from 'rxjs';
+import { IGif } from '../models/gif.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GiphyService {
+  private http = inject(HttpClient);
+
+  private url: string;
+
+  constructor() {
+    this.url = config.URL_API;
+  }
+
+  getTrendingGifs(
+    page: number = 1,
+    size: number = 20
+  ): Observable<IGif[]> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.get<{ data: any[] }>(this.url + 'gifs/trending', {
+      headers: headers,
+      params: {
+        api_key: config.API_KEY,
+        limit: size.toString(),
+        offset: ((page - 1) * size).toString(),
+        //rating: 'g',
+        //bundle: 'messaging_non_clips',
+      },
+    }).pipe(map( (res) => res.data ));
+  }
+
+  getTrendingTags(): Observable<string[]> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+    return this.http.get<{ data: any[] }>(this.url + 'trending/searches', {
+      headers: headers,
+      params: {
+        api_key: config.API_KEY,
+      },
+    }).pipe(map( (res) => res.data ));
+  }
+}
