bb6fb7cfcd75462a64b353b82c9f9cb9b71e3b03
josephsb <jguku2002@gmail.com>
2024-04-26 22:46:36 -0500

    add loader

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,17 @@ import { GiphyService } from '../../services/giphy.service';
 import { CommonModule } from '@angular/common';
 import { GifCardComponent } from '../../components/gif-card/gif-card.component';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, GifCardComponent, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    GifCardComponent,
+    ReactiveFormsModule,
+    NgxSkeletonLoaderModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   providers: [GiphyService],
@@ -16,6 +22,8 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 export class HomeComponent {
   giphyService = inject(GiphyService);
 
+  loader_gifs = signal(true);
+  loader_tags = signal(true);
   gifs = signal<IGif[]>([]);
   tags = signal<string[]>([]);
 
@@ -25,7 +33,7 @@ export class HomeComponent {
       Validators.required,
       Validators.pattern('^\\S.*$'),
       Validators.minLength(3),
-    ]
+    ],
   });
 
   ngOnInit() {
@@ -34,32 +42,40 @@ export class HomeComponent {
   }
 
   getGifs() {
+    this.loader_gifs.set(true);
     this.giphyService.getTrendingGifs().subscribe((data) => {
       this.gifs.update((prev) => [...prev, ...data]);
+      this.loader_gifs.set(false);
     });
   }
 
   getTrendingTags() {
+    this.loader_tags.set(true);
     this.giphyService.getTrendingTags().subscribe((data) => {
       this.tags.set(data);
+      this.loader_tags.set(false);
     });
   }
 
   searchTerm(value: string) {
+    this.loader_gifs.set(true);
     this.giphyService.searchGif(value).subscribe((data) => {
       this.gifs.set(data);
+      this.loader_gifs.set(false);
     });
   }
 
   searchGif(e: Event) {
+    this.loader_gifs.set(true);
     const input = e.target as HTMLInputElement;
     this.giphyService.searchGif(input.value).subscribe((data) => {
       this.gifs.set(data);
+      this.loader_gifs.set(false);
     });
   }
 
   cleanInputSearch() {
-    this.inputSearchCtrl.setValue("");
+    this.inputSearchCtrl.setValue('');
     this.gifs.set([]);
     this.getGifs();
   }
